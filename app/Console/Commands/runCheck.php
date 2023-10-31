08d7c849fa076159389dff72aa8bddd3a50f557c
manjurulhasan <manjurul.hasan@selise.ch>
2023-10-31 17:52:10 +0600

    header updated.

--- app/Console/Commands/runCheck.php
+++ app/Console/Commands/runCheck.php
@@ -95,15 +95,14 @@ class runCheck extends Command
         //      'https://shajib.ch'
         //  ];
 
-        $websites = $this->getSites();
-         $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
-         $referer = 'https://vicafe.ch';
+         $websites = $this->getSites();
+         $headers = [
+             'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+             'Referer' => 'https://erp.vicafe.ch',
+         ];
 
         $client = new Client([
-            'headers' => [
-                'User-Agent' => $userAgent,
-                'referer' => $referer
-            ]
+            'headers' => $headers
         ]);
 
         $requests = function ($websites) use ($client) {
