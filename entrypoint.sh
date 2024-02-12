4c0f530a6dc94ddbac9a7f439843e0fb382a6192
manjurulhasan <manjurul.hasan@selise.ch>
2024-02-12 14:24:34 +0600

    docker env updated.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,9 @@ set -e
 if [ ! -f ".env" ]; then
     echo "Creating env file for env"
     cp .env.example .env
+    php artisan migrate
+    php artisan optimize:clear
+    php artisan optimize
 fi
 # Fix files ownership.
 chown -R www-data:www-data .
